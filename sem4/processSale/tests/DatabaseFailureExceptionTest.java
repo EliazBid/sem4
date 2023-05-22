88b17a594676a92c60575b5dd24d314d89174b24
EliazBid <eliaz@kth.se>
2023-05-22 11:47:23 +0200

    tests

---
+++ sem4/processSale/tests/DatabaseFailureExceptionTest.java
@@ -0,0 +1,11 @@
+package processSale.tests;
+
+import org.junit.Test;
+
+public class DatabaseFailureExceptionTest {
+    
+    @Test
+    public void testDatabaseFailureException() {
+        
+    }
+}
---
+++ sem4/processSale/tests/ItemNotFoundExceptionTest.java
@@ -0,0 +1,11 @@
+package processSale.tests;
+
+import org.junit.Test;
+
+public class ItemNotFoundExceptionTest {
+    
+    @Test
+    public void testItemNotFoundException() {
+        
+    }
+}
