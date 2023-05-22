e8377d4ac7432743fd8f179b22b911c760f4607e
EliazBid <eliaz@kth.se>
2023-05-22 15:39:33 +0200

    added comment and made method more clear

--- sem4/processSale/model/Sale.java
+++ sem4/processSale/model/Sale.java
@@ -122,8 +122,12 @@ public class Sale {
 
 	}
 
+	/**
+	 * Adds a list of sale observers to the sale.
+	 * @param observers list of sale observers
+	 */
 	public void addSaleObservers(List<SaleObserver> observers) {
-		saleObservers.addAll(observers);
+		this.saleObservers = observers;
 	}
 
 	/**
